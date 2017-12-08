4c2440ab9eb00297c85f4a5ea74e8c0aa0b08401
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-08 10:37:59 -0600

    combo show fns into one

--- assets/js/animals.js
+++ assets/js/animals.js
@@ -1,39 +1,13 @@
 "use strict";
 
-function loadAnimals(callback, filename) {
+module.exports.loadAnimals = (callback, filename, divId) => {
     const loader = new XMLHttpRequest();
     
     loader.addEventListener("load", function() {
         let animals = JSON.parse(this.responseText);
-        callback(animals);
+        callback(animals, divId);
     });
 
     loader.open("GET", `/assets/json/${filename}`, true);
     loader.send();
-}
-
-function loadCarnivores(callback) {
-    const loader = new XMLHttpRequest();
-
-    loader.addEventListener("load", function() {
-        let carnivores = JSON.parse(this.responseText);
-        callback(carnivores);
-    });
-
-    loader.open("GET", "/assets/json/carnivores.json", true);
-    loader.send();
-}
-
-function loadHerbivores(callback) {
-    const loader = new XMLHttpRequest();
-
-    loader.addEventListener("load", function() {
-        let herbivores = JSON.parse(this.responseText);
-        callback(herbivores);
-    });
-
-    loader.open("GET", "/assets/json/herbivores.json", true);
-    loader.send();
-}
-
-module.exports = {loadCarnivores, loadHerbivores, loadAnimals};
+};
